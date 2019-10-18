1268ca4f3f6ac861c2a7021885927ba99ea94af3
Claudemir Ramos Ferreira (BR-MIS) <claudemirferreira@7bit.com.br>
2019-10-18 11:06:24 -0400

    commit initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RelatorioComponent } from './components/relatorio/relatorio.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+
+const appRoutes: Routes = [
+  { path: 'relatorio', component: RelatorioComponent },
+  { path: 'usuario',      component: UsuarioComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelatorioComponent,
+    UsuarioComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    NgxExtendedPdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
